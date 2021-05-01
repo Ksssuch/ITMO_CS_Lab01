f3be9462ec6d0552b77a0b1442d81f2594abf98e
Ksssuch <ksssuch@mail.ru>
2021-05-01 20:03:55 +0300

    Greeting programm

---
+++ Greetings/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Greetings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName;
+            Console.WriteLine("Insert your name");
+            myName = Console.ReadLine();
+            Console.WriteLine("hello {0} " , myName);
+        }
+    }
+}
